45439ebe468bbbc02dccdf521f2bacf1509c12df
LucasBoesso <lucasboesso66@gmail.com>
2023-02-16 17:22:59 -0300

    OK

--- App2.py
+++ App2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 
 st.write("# Novo aplicativo")
 
@@ -6,3 +7,7 @@ btn1 = st.button("Novo botão")
 
 if btn1 == True:
     st.write("### Sua aplicação está funcionando")
+
+
+l= np.random.randint(5,20,10)
+st.write(l)
